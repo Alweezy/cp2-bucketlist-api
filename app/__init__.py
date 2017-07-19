1838f3bc926f9f50ffa06a45d8d9b0a680cb76c0
Alweezy <alvinmutisya@gmail.com>
2017-07-19 11:32:16 +0300

    Update endpoints and routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,25 +9,26 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    from app.models import BucketList
+    from app.models import BucketList, BucketListItem, User
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    @app.route('/bucketlists/', methods=['POST', 'GET'])
-    def bucketlists():
+    @app.route('/api/v1/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists(user_id):
         if request.method == "POST":
             name = str(request.data.get('name', ''))
             if name:
-                bucketlist = BucketList(name=name)
+                bucketlist = BucketList(name=name, created_by=user_id)
                 bucketlist.save()
                 response = jsonify({
                     'id': bucketlist.id,
                     'name': bucketlist.name,
                     'date_created': bucketlist.date_created,
-                    'date_modified': bucketlist.date_modified
+                    'date_modified': bucketlist.date_modified,
+                    'created_by': user_id
                 })
                 response.status_code = 201
                 return response
@@ -48,7 +49,7 @@ def create_app(config_name):
             response.status_code = 200
             return response
 
-    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    @app.route('/api/v1/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
     def bucketlist_manipulation(id, **kwargs):
         # retrieve a buckelist using it's ID
         bucketlist = BucketList.query.filter_by(id=id).first()
@@ -70,17 +71,33 @@ def create_app(config_name):
                 'id': bucketlist.id,
                 'name': bucketlist.name,
                 'date_created': bucketlist.date_created,
-                'date_modified': bucketlist.date_modified
+                'date_modified': bucketlist.date_modified,
+                'created_by': bucketlist.created_by
             })
-            response.status_code = 200
+            response.status_code = 201
             return response
         else:
             # GET
+            items = BucketListItem.query.filter_by(bucketlist_id=id)
+            bucketlist_items = []
+            for item in items:
+                data = {
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "done": item.done
+                }
+                bucketlist_items.append(data)
+
             response = jsonify({
                 'id': bucketlist.id,
                 'name': bucketlist.name,
                 'date_created': bucketlist.date_created,
-                'date_modified': bucketlist.date_modified
+                'date_modified': bucketlist.date_modified,
+                'created_by': bucketlist.created_by,
+                'items': bucketlist_items
+
             })
             response.status_code = 200
             return response
