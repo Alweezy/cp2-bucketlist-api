65b46b026c30a8b845b2e9aa33a1c7b6e7322639
Alweezy <alvinmutisya@gmail.com>
2017-07-19 20:51:33 +0300

    Provide alternative secret key

--- instance/config.py
+++ instance/config.py
@@ -1,11 +1,14 @@
 import os
 
+if not os.getenv('SECRET_KEY'):
+    os.environ['SECRET_KEY'] = str(os.urandom(24))
+
 
 class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
