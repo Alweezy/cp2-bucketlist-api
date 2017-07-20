5c117433e1216acbc0e2f53104fab66ae05e86c5
Alweezy <alvinmutisya@gmail.com>
2017-07-20 19:43:44 +0300

    Change testing database to sqlite

--- instance/config.py
+++ instance/config.py
@@ -18,7 +18,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.db'
     DEBUG = True
 
 
