fdea8b1f076b18d6d3fc2ef09e9c444741a694f4
Alweezy <alvinmutisya@gmail.com>
2017-07-21 12:31:23 +0300

    Remove unnecessary imports

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import db, create_app
-from app import models
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
