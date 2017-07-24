11ab54957b2652fea3bbd6e23411a6fba6d3cc9c
Alweezy <alvinmutisya@gmail.com>
2017-07-24 10:27:17 +0300

    [Feature] Implement bucketlist items

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, make_response
 
 
 from instance.config import app_config
@@ -105,6 +105,109 @@ def create_app(config_name):
             })
             response.status_code = 200
             return response
+
+    @app.route('/api/v1/bucketlists/<int:id>/items/', methods=['GET', 'POST'])
+    @evaluate_auth
+    def bucketlist_items(id, user_id, *args, **kwargs):
+        bucketlist = BucketList.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+
+        if request.method == 'GET':
+            items = BucketListItem.query.filter_by(bucketlist_id=id)
+            results = []
+
+            for item in items:
+                data = {
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "bucketlist_id": item.bucketlist_id,
+                    "done": item.done
+                }
+                results.append(data)
+            return make_response(jsonify(results)), 200
+        elif request.method == 'POST':
+            name = str(request.data.get("name", ""))
+            if name:
+                item = BucketListItem(name=name, bucketlist_id=id)
+                item.save()
+                response = jsonify({
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "bucketlist_id": item.bucketlist_id,
+                    "done": item.done,
+                    "created_by": user_id
+                    })
+                return make_response(response), 201
+
+    @app.route('/api/v1/bucketlists/<int:id>/items/<int:item_id>', methods=['GET', 'PUT', 'DELETE'])
+    @evaluate_auth
+    def bucketlist_item_manipulation(id, item_id, user_id, *atgs, **kwargs):
+        item = BucketListItem.query.filter_by(bucketlist_id=id).filter_by(
+            id=item_id).first()
+        if not item:
+            abort(404)
+
+        if request.method == "DELETE":
+            item.delete()
+            response = jsonify({
+                "message": "Item {} has been successfully deleted"
+                .format(item.name)})
+            response.status_code = 204
+            return response
+
+        elif request.method == "PUT":
+            name = str(request.data.get("name", ""))
+            done = str(request.data.get("done", ""))
+            if name:
+                item.name = name
+                item.save()
+                response = jsonify({
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "bucketlist_id": item.bucketlist_id,
+                    "created_by": user_id,
+                    "done": item.done})
+
+                return make_response(response), 201
+
+            elif done:
+                item.done = done
+                item.save()
+                response = jsonify({
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "bucketlist_id": item.bucketlist_id,
+                    "created_by": user_id,
+                    "done": item.done})
+
+                return make_response(response), 200
+
+        elif request.method == "GET":
+            all_items = BucketListItem.query.filter_by(id=item_id)
+            results = []
+
+            for item in all_items:
+                obj = {
+                    "id": item.id,
+                    "name": item.name,
+                    "date_created": item.date_created,
+                    "date_modified": item.date_modified,
+                    "bucketlist_id": item.bucketlist_id,
+                    "done": item.done
+                }
+                results.append(obj)
+
+            return make_response(jsonify(results)), 200
+
     from app.auth import authenticate_blueprint
     app.register_blueprint(authenticate_blueprint)
     return app
