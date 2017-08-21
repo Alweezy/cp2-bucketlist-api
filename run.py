e7f46ec3a9b529f6c0e8a701d47abc6b77961d97
Alweezy <alvinmutisya@gmail.com>
2017-08-21 19:06:23 +0300

    [Fix]Adjust application requirements

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import os
-
-from app import create_app
-
-config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
+from app.views import app
 
 if __name__ == '__main__':
     app.run()
